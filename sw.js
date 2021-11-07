299076950d32fc0c630925cb3645f12df4300844
Ruijia Brooks Niu <brooksnew92@gmail.com>
2021-11-07 06:06:53 -0800

    minor issue & im going to bed. things worked out

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ event.respondWith(
   caches.open(CACHE_NAME).then(function (cache){
     return cache.match(event.request).then(function (response) {
       // return if exists, add if DNE
-      return resposne || fetch(event.request).then(function (response) {
+      return response || fetch(event.request).then(function (response) {
         cache.put(event.request, response.clone());
         return response;
       })
